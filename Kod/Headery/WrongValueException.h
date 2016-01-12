270c31b518bf93f819d41eac43427c3e38193c3f
GSource2015 <gsource2015@gmail.com>
2016-01-12 10:07:37 +0100

    Create WrongValueException.h

---
+++ Kod/Headery/WrongValueException.h
@@ -0,0 +1,15 @@
+#ifndef _WRONGVALUEEXCEPTION_H_
+#define _WRONGVALUEEXCEPTION_H_
+#include <iostream>
+
+class WrongValueException : public std::exception {
+public:
+	WrongValueException(const double val) throw();
+	~WrongValueException() throw();
+	const char* what() const throw();
+	double getValue() const throw();
+private:
+	double _value;
+};
+
+#endif
