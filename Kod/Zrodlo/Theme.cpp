8229bbe66345acbf30c066984b6d596eb6e78e95
GSource2015 <gsource2015@gmail.com>
2016-01-19 02:20:22 +0100

    Update Theme.cpp

--- Kod/Zrodlo/Theme.cpp
+++ Kod/Zrodlo/Theme.cpp
@@ -1,29 +1,41 @@
 #include "Theme.h"
 
-Theme::Theme(){
 
+
+Theme::Theme(){
 }
 //Theme::Theme(const Theme& source);
-//Theme& Theme::operator=(const Theme& source);
+Theme& Theme::operator=(const Theme& source){
+	_background = source._background;
+	_walls =  source._walls;
+	
+	for(int i = 0; i < 6; ++i){
+		_floors[i] = source._floors[i];
+	}
+	isDefault = source.isDefault;
+	_mtheme = source._mtheme;;
+	_name = source._name;	
+	
+}
 
-Theme& Theme::LoadBackground(const wxColour& background){
+Theme& Theme::loadBackground(const wxColour& background){
 	_background = background;
 	return *this;
 }
-Theme& Theme::LoadBackground(unsigned char R, unsigned char G, unsigned char B){
+Theme& Theme::loadBackground(unsigned char R, unsigned char G, unsigned char B){
 	_background.Set(R, G, B);
 	return *this;
 }
-Theme& Theme::LoadWalls(const wxColour& walls){
+Theme& Theme::loadWalls(const wxColour& walls){
 	_walls = walls;
 	return *this;
 }
-Theme& Theme::LoadWalls(unsigned char R, unsigned char G, unsigned char B){
+Theme& Theme::loadWalls(unsigned char R, unsigned char G, unsigned char B){
 	_walls.Set(R, G, B);
 	return *this;
 }
-Theme& Theme::LoadFloor(const int i, const wxColour& floor){
-	if (i > 5)
+Theme& Theme::loadFloor(const int i, const wxColour& floor){
+	if (i > 6)
 		throw OutOfRangeIndexException(i);
 	else if (i < 0)
 		throw NegativeIndexException(i);
@@ -32,7 +44,7 @@ Theme& Theme::LoadFloor(const int i, const wxColour& floor){
 		return *this;
 	}
 }
-Theme& Theme::LoadFloor(const int i, const unsigned char R, const unsigned char G, const unsigned char B){
+Theme& Theme::loadFloor(const int i, const unsigned char R, const unsigned char G, const unsigned char B){
 	if (i > 5)
 		throw OutOfRangeIndexException(i);
 	else if (i < 0)
@@ -44,9 +56,10 @@ Theme& Theme::LoadFloor(const int i, const unsigned char R, const unsigned char 
 }
 Theme& Theme::setTheme(main_theme new_theme){
 	_mtheme = new_theme;
+	return *this;
 }
 
-wxColour& Theme::getBacground(){
+wxColour& Theme::getBackground(){
 	return _background;
 }
 wxColour& Theme::getWalls(){
@@ -63,4 +76,33 @@ wxColour& Theme::getFloor(int number){
 }
 Theme::main_theme Theme::getTheme() const{
 	return _mtheme;
+}
+
+Theme& Theme::setName(const char *name){
+	_name = name;
+	return *this;
+}
+
+Theme& Theme::setName(std::string name){
+	_name = name;
+	return *this;
+}
+
+string  Theme::getName(){
+	return _name;
+}
+
+Theme& Theme::loadFloor(const int i, unsigned long colour){
+	_floors[i] = wxColour(colour);
+	return *this;
+}
+
+Theme& Theme::loadBackground(unsigned long colour){
+	_background = wxColour(colour);
+	return *this;
+}
+
+Theme& Theme::loadWalls(unsigned long colour){
+	_walls = wxColour(colour);
+	return *this;
 }
